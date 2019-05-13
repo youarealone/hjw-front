14eb58a9cba19ccb8b5775f3fb422f1e1c9245f5
ssoso27 <sohi8290@naver.com>
2019-05-13 12:59:40 +0900

    [F17] 메인화면 구현
    - 툴바 구현

---
+++ app/src/main/java/com/example/hjw_front/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hjw_front;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+}

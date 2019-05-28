e663163f1e28afbb7c26b9995c118490315c144c
chimaek <pipiru100@gmail.com>
2019-05-28 23:29:32 +0900

    [F23] 스플래시(로딩)화면 구현

---
+++ app/src/main/java/com/example/hjw_front/splash.java
@@ -0,0 +1,45 @@
+package com.example.hjw_front;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // xml 에서 테마 적용이 안되는 버전이 있을경우 강제 코드 적용
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        // 애니메이션 적용
+        ImageView imageView = findViewById(R.id.splash_img);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.alpha);
+        imageView.startAnimation(animation);
+
+        //지연시간을 위한 핸들러 추가
+        Handler handler = new Handler();
+        handler.postDelayed(new splash_handler(), 1800);
+
+    }
+
+    // 핸들러 함수 만들기
+    private class splash_handler implements Runnable {
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            splash.this.finish();
+        }
+    }
+}

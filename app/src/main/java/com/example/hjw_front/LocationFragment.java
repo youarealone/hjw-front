905ab3daf9f868e99a5cc8a1cd9866789c9e6764
ssoso27 <sohi8290@gmail.com>
2019-08-28 08:00:06 +0900

    [F31] 편의시설 위치 화면 구현
    - 카카오맵 연동 성공

--- app/src/main/java/com/example/hjw_front/LocationFragment.java
+++ app/src/main/java/com/example/hjw_front/LocationFragment.java
@@ -1,5 +1,7 @@
 package com.example.hjw_front;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,9 +10,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapView;
+
 public class LocationFragment extends Fragment {
+    private Activity activity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof Activity)
+            this.activity = (Activity) context;
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view =  inflater.inflate(R.layout.fragment_location, container, false);
+
+        MapView mapView = new MapView(this.activity);
+
+        ViewGroup mapViewContainer = view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        return view;
     }
 }

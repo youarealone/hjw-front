03ec3eca5ef7729a756a3f450f3da1a695465a23
ssoso27 <sohi8290@gmail.com>
2019-08-28 05:52:24 +0900

    [F29] 검색 화면 구현
    - 해시태그 구현

--- app/src/main/java/com/example/hjw_front/SearchFragment.java
+++ app/src/main/java/com/example/hjw_front/SearchFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SearchFragment extends Fragment {
     @Override
@@ -18,6 +20,23 @@ public class SearchFragment extends Fragment {
         InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
+        setHashtags(view);
+
         return view;
+    }
+
+    private void setHashtags(View view) {
+        String[] hashtags = {
+                "#제주핫플", "#제주여행", "#바다", "#함덕", "#세화", "#애월", "#제주오름", "#제주카페", "제주혼행"
+        };
+
+        LinearLayout container = view.findViewById(R.id.container_hashtags);
+
+        for (String hashtag: hashtags) {
+            TextView textView = new TextView(getContext());
+            textView.setText(hashtag);
+
+            container.addView(textView);
+        }
     }
 }

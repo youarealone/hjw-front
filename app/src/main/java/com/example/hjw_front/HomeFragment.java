bb541897e2ee7dcffea41dc743664afea1dfa3df
ssoso27 <sohi8290@gmail.com>
2019-08-28 05:43:19 +0900

    [F29] 검색 화면 구현
    - 검색버튼 클릭 시 회면 넘어감

--- app/src/main/java/com/example/hjw_front/HomeFragment.java
+++ app/src/main/java/com/example/hjw_front/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.hjw_front;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,7 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.hjw_front.utils.FragmentChanger;
+
 public class HomeFragment extends Fragment {
+    private FragmentChanger fragmentChanger = null;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        fragmentChanger = new FragmentChanger(getActivity().getSupportFragmentManager());
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -17,7 +28,7 @@ public class HomeFragment extends Fragment {
         view.findViewById(R.id.container_search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "얍", Toast.LENGTH_LONG).show();
+                fragmentChanger.changeFragment(new SearchFragment());
             }
         });
 
---
+++ app/src/main/java/com/example/hjw_front/SearchFragment.java
@@ -0,0 +1,23 @@
+package com.example.hjw_front;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+
+public class SearchFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        return view;
+    }
+}

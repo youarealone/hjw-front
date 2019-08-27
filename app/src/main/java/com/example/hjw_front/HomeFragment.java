18656ac6120369729c00a796a9c8370f8dad5a4a
ssoso27 <sohi8290@gmail.com>
2019-08-28 04:37:15 +0900

    [F27] 검색 화면 구현
    - 검색 온클릭 구현

--- app/src/main/java/com/example/hjw_front/HomeFragment.java
+++ app/src/main/java/com/example/hjw_front/HomeFragment.java
@@ -7,10 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        view.findViewById(R.id.container_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "얍", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }

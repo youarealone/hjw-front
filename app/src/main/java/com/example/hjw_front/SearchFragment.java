dd760d02b826d4f5ae89b8d2cabc347c45d2ccdd
ssoso27 <sohi8290@gmail.com>
2019-08-28 07:04:20 +0900

    [F29] 검색 화면 구현
    - 많이 검색한 단어 구현

--- app/src/main/java/com/example/hjw_front/SearchFragment.java
+++ app/src/main/java/com/example/hjw_front/SearchFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,6 +26,7 @@ public class SearchFragment extends Fragment {
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
         setHashtags(view);
+        setSearchKeyword(view);
 
         return view;
     }
@@ -43,13 +45,39 @@ public class SearchFragment extends Fragment {
             textView.setTextSize(20);
             textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
 
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+            FlowLayout.LayoutParams lp = new FlowLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(0,0,30,30);
+            lp.setMargins(0,0,30,20);
             textView.setLayoutParams(lp);
 
             container.addView(textView);
+        }
+    }
+
+    private void setSearchKeyword(View view) {
+        String[] keywords = {
+                "제주 가을여행지", "제주 가볼만한곳", "제주 핑크뮬리", "제주도 관광지"
+        };
+
+        FlowLayout container = view.findViewById(R.id.container_keywords);
+
+        for (String keyword: keywords) {
+            Button button = new Button(getContext());
+            button.setText(keyword);
+            button.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+            button.setTextSize(15);
+            button.setTypeface(button.getTypeface(), Typeface.BOLD);
+            button.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.blue_round_button));
+            button.setPadding(20, 10, 20, 10);
+
+            FlowLayout.LayoutParams lp = new FlowLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(0,0,30,30);
+            button.setLayoutParams(lp);
+
+            container.addView(button);
         }
     }
 }

80577352abe4d9828db9a97a92fc4458d98d95da
ssoso27 <sohi8290@gmail.com>
2019-10-13 22:24:11 +0900

    [F38] 일정 추가 팝업 구현
    - 추가 취소 버튼 이벤트 연결

--- app/src/main/java/com/example/hjw_front/fragments/AddScheduleDialogFragment.java
+++ app/src/main/java/com/example/hjw_front/fragments/AddScheduleDialogFragment.java
@@ -10,10 +10,12 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.hjw_front.R;
 
-public class AddScheduleDialogFragment extends DialogFragment {
+public class AddScheduleDialogFragment extends DialogFragment implements View.OnClickListener {
     public static final String TAG = "add_schedule";
 
     @Nullable
@@ -21,8 +23,28 @@ public class AddScheduleDialogFragment extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_add_schedule, container, false);
 
+        setCancelable(false);
 
+        view.findViewById(R.id.btn_add_schedule).setOnClickListener(this);
+        view.findViewById(R.id.btn_dismiss_schedule_dialog).setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_add_schedule:
+                Toast.makeText(getContext(), "애드", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.btn_dismiss_schedule_dialog:
+                Toast.makeText(getContext(), "취소", Toast.LENGTH_LONG).show();
+                dismiss();
+                break;
+
+            default:
+                break;
+        }
     }
 }

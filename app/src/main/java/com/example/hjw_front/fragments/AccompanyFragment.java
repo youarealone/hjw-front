a0acb9040847e271aea46bc2d857ac29466a2652
ssoso27 <sohi8290@gmail.com>
2019-10-26 01:31:26 +0900

    [F42] 동행 페이지 구현
    - mock데이터 연결
    - Glide 라이브러리 추가

--- app/src/main/java/com/example/hjw_front/fragments/AccompanyFragment.java
+++ app/src/main/java/com/example/hjw_front/fragments/AccompanyFragment.java
@@ -7,28 +7,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.hjw_front.R;
+import com.example.hjw_front.adapters.AccompanyAdapter;
+import com.example.hjw_front.vo.AccompanyPostVO;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
 public class AccompanyFragment extends Fragment {
-    String mockName, mockEmail, mockPhotoURL, mockUid;
     private FirebaseUser currentUser;
 
     public AccompanyFragment() {
         this.currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        this.mockEmail = currentUser.getEmail();
-        this.mockName = currentUser.getDisplayName();
-        this.mockPhotoURL = currentUser.getPhotoUrl().toString();
-        this.mockUid = currentUser.getUid();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_accompany, container, false);
+
+        ArrayList<AccompanyPostVO> posts = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            AccompanyPostVO post = new AccompanyPostVO(
+                    i+""
+                    , currentUser.getUid()
+                    , currentUser.getPhotoUrl().toString()
+                    , currentUser.getDisplayName()
+                    , "2018.05.05"
+                    , "2018.07.01"
+                    , "#동행"
+                    , "우후루꾸루후 슈퍼스타"
+            );
+            posts.add(post);
+        }
+
+        AccompanyAdapter adapter = new AccompanyAdapter(posts);
+        ListView listView = view.findViewById(R.id.lv_accompany_posts);
+        listView.setAdapter(adapter);
+
         return view;
     }
 }

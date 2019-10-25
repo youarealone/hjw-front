74f472e6494c86b5d28546e71b80da4a7c425b88
ssoso27 <sohi8290@gmail.com>
2019-10-26 00:10:23 +0900

    [F42] 동행 페이지 구현
    - 동행게시글 VO 생성
    - 동행게시글 아이템 생성

--- app/src/main/java/com/example/hjw_front/fragments/AccompanyFragment.java
+++ app/src/main/java/com/example/hjw_front/fragments/AccompanyFragment.java
@@ -9,8 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hjw_front.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccompanyFragment extends Fragment {
+    String mockName, mockEmail, mockPhotoURL, mockUid;
+    private FirebaseUser currentUser;
+
+    public AccompanyFragment() {
+        this.currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        this.mockEmail = currentUser.getEmail();
+        this.mockName = currentUser.getDisplayName();
+        this.mockPhotoURL = currentUser.getPhotoUrl().toString();
+        this.mockUid = currentUser.getUid();
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {

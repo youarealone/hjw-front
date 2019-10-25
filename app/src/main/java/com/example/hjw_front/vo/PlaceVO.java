aa2e384e64ca2f66114aea60d40f7e3b16e3c6b8
chimaek <pipiru100@gmail.com>
2019-10-26 00:49:25 +0900

    관광지 할인을 위한 VO생성

---
+++ app/src/main/java/com/example/hjw_front/vo/PlaceVO.java
@@ -0,0 +1,40 @@
+package com.example.hjw_front.vo;
+
+public class PlaceVO {
+    private String placeName;
+    private String placeAdress;
+    private String placeImage;
+    private int placePrice;
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPlaceAdress() {
+        return placeAdress;
+    }
+
+    public void setPlaceAdress(String placeAdress) {
+        this.placeAdress = placeAdress;
+    }
+
+    public String getPlaceImage() {
+        return placeImage;
+    }
+
+    public void setPlaceImage(String placeImage) {
+        this.placeImage = placeImage;
+    }
+
+    public int getPlacePrice() {
+        return placePrice;
+    }
+
+    public void setPlacePrice(int placePrice) {
+        this.placePrice = placePrice;
+    }
+}

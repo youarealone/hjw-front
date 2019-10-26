a1c37f5e6a09cb1e01dc42cb4033e8c64942c6b1
ssoso27 <sohi8290@gmail.com>
2019-10-26 15:24:36 +0900

    [F44] 동행 게시글 작성 페이지 추가
    - 시작날짜, 마지막날짜에 데이트피커 추가

--- app/src/main/java/com/example/hjw_front/fragments/WriteAccompanyFragment.java
+++ app/src/main/java/com/example/hjw_front/fragments/WriteAccompanyFragment.java
@@ -1,5 +1,6 @@
 package com.example.hjw_front.fragments;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,89 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.hjw_front.R;
 
-public class WriteAccompanyFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class WriteAccompanyFragment extends Fragment implements View.OnClickListener {
+    private TextView tvStartDate;
+    private TextView tvEndDate;
+
+    private Calendar startCalendar, endCalendar;
+    private DatePickerDialog.OnDateSetListener startDatePicker, endDatePicker;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_write_accompany, container, false);
+
+        tvStartDate = view.findViewById(R.id.tv_write_accompany_start);
+        tvEndDate = view.findViewById(R.id.tv_write_accompany_end);
+
+        tvStartDate.setOnClickListener(this);
+        tvEndDate.setOnClickListener(this);
+
+        startCalendar = Calendar.getInstance();
+        startDatePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                startCalendar.set(Calendar.YEAR, year);
+                startCalendar.set(Calendar.MONTH, month);
+                startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "yy.MM.dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+                tvStartDate.setText(sdf.format(startCalendar.getTime()));
+            }
+        };
+
+        endCalendar = Calendar.getInstance();
+        endDatePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                endCalendar.set(Calendar.YEAR, year);
+                endCalendar.set(Calendar.MONTH, month);
+                endCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "yy.MM.dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+                tvEndDate.setText(sdf.format(endCalendar.getTime()));
+            }
+        };
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_write_accompany_start:
+                new DatePickerDialog(getContext()
+                        , startDatePicker
+                        , startCalendar.get(Calendar.YEAR)
+                        , startCalendar.get(Calendar.MONTH)
+                        , startCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+                break;
+
+            case R.id.tv_write_accompany_end:
+                new DatePickerDialog(getContext()
+                        , endDatePicker
+                        , endCalendar.get(Calendar.YEAR)
+                        , endCalendar.get(Calendar.MONTH)
+                        , endCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+                break;
+
+            default:
+                break;
+        }
     }
 }

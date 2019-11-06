a819dd7f9353d4fc3a4556a0488611bb391f1877
chimaek <pipiru100@gmail.com>
2019-11-06 13:18:03 +0900

    라이브러리 AndroidX로 업데이트

--- app/src/main/java/com/example/hjw_front/fragments/LocationFragment.java
+++ app/src/main/java/com/example/hjw_front/fragments/LocationFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,7 +21,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.HashSet;
 import java.util.List;
 
 import noman.googleplaces.NRPlaces;
@@ -40,7 +39,7 @@ public class LocationFragment extends Fragment implements
 
     private Activity activity;
     private Button btnCurrent;
-
+    private TextView tv_addr;
 
     @Override
     public void onAttach(Context context) {
@@ -52,36 +51,25 @@ public class LocationFragment extends Fragment implements
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
 
-        tvDoro = view.findViewById(R.id.tv_addr_doro);
+        tv_addr = view.findViewById(R.id.tv_addr_doro);
         btnCurrent = view.findViewById(R.id.btn_current);
 
-
-
         return view;
-    }
-
-
-
-    }
-
-    private void setListener() {
-        // 현재위치 가져오기
-        btnCurrent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "현재 위치를 가져오고 있습니다...", Toast.LENGTH_LONG).show();
-                getLocation();
-            }
-        });
     }
 
 
     // 구글 맵 사용
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        LatLng SEOUL = new LatLng(37.56, 126.97);
 
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
     }
 
     @Override
@@ -96,34 +84,34 @@ public class LocationFragment extends Fragment implements
 
     @Override
     public void onPlacesSuccess(final List<Place> places) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                for (noman.googleplaces.Place place : places) {
-
-                    LatLng latLng
-                            = new LatLng(place.getLatitude()
-                            , place.getLongitude());
-
-                    String markerSnippet = getCurrentAddress(latLng);
-
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(latLng);
-                    markerOptions.title(place.getName());
-                    markerOptions.snippet(markerSnippet);
-                    Marker item = mMap.addMarker(markerOptions);
-                    previous_marker.add(item);
-
-                }
-
-                //중복 마커 제거
-                HashSet<Marker> hashSet = new HashSet<Marker>();
-                hashSet.addAll(previous_marker);
-                previous_marker.clear();
-                previous_marker.addAll(hashSet);
-
-            }
-        });
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (noman.googleplaces.Place place : places) {
+//
+//                    LatLng latLng
+//                            = new LatLng(place.getLatitude()
+//                            , place.getLongitude());
+//
+//                    String markerSnippet = getCurrentAddress(latLng);
+//
+//                    MarkerOptions markerOptions = new MarkerOptions();
+//                    markerOptions.position(latLng);
+//                    markerOptions.title(place.getName());
+//                    markerOptions.snippet(markerSnippet);
+//                    Marker item = mMap.addMarker(markerOptions);
+//                    previous_marker.add(item);
+//
+//                }
+//
+//                //중복 마커 제거
+//                HashSet<Marker> hashSet = new HashSet<Marker>();
+//                hashSet.addAll(previous_marker);
+//                previous_marker.clear();
+//                previous_marker.addAll(hashSet);
+//
+//            }
+//        });
     }
 
     @Override
@@ -131,8 +119,7 @@ public class LocationFragment extends Fragment implements
 
     }
 
-    public void showPlaceInformationHospital(LatLng location)
-    {
+    public void showPlaceInformationHospital(LatLng location) {
         mMap.clear();//지도 클리어
 
         if (previous_marker != null)
@@ -142,13 +129,13 @@ public class LocationFragment extends Fragment implements
                 .listener(LocationFragment.this)
                 .key("AIzaSyBnKCVMvSkb2fk98klhkwbOTSQl_n2lYlk")
                 .latlng(location.latitude, location.longitude)//현재 위치
-                .radius(3000) //500 미터 내에서 검색
+                .radius(3000) // 3000 미터 내에서 검색
                 .type(PlaceType.HOSPITAL) //병원
                 .build()
                 .execute();
     }
-    public void showPlaceInformationPharmacy(LatLng location)
-    {
+
+    public void showPlaceInformationPharmacy(LatLng location) {
         mMap.clear();//지도 클리어
 
         if (previous_marker != null)
@@ -158,7 +145,7 @@ public class LocationFragment extends Fragment implements
                 .listener(LocationFragment.this)
                 .key("AIzaSyBnKCVMvSkb2fk98klhkwbOTSQl_n2lYlk")
                 .latlng(location.latitude, location.longitude)//현재 위치
-                .radius(3000) //500 미터 내에서 검색
+                .radius(3000) // 3000미터 내에서 검색
                 .type(PlaceType.PHARMACY) //약국
                 .build()
                 .execute();

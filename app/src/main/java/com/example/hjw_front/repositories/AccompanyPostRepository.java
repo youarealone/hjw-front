9f897e09ca718803e9a0e7d3c5b7c7aa9bcba74d
ssoso27 <sohi8290@gmail.com>
2019-11-14 18:50:09 +0900

    [F51] 동행 게시글 작성 기능 추가
    - 주석 삭제

--- app/src/main/java/com/example/hjw_front/repositories/AccompanyPostRepository.java
+++ app/src/main/java/com/example/hjw_front/repositories/AccompanyPostRepository.java
@@ -3,16 +3,11 @@ package com.example.hjw_front.repositories;
 import android.util.Log;
 
 import com.example.hjw_front.vo.AccompanyPostVO;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import androidx.annotation.NonNull;
 
 public class AccompanyPostRepository {
     private final String TAG = "[AccompanyPostRepo]";
@@ -31,11 +26,11 @@ public class AccompanyPostRepository {
     public void create(AccompanyPostVO accompanyPostVO) {
         Map<String, Object> map = new HashMap<>();
         map.put("uid", accompanyPostVO.getUid());
-//        map.put("photoURL", accompanyPostVO.getPhotoURL());
-//        map.put("username", accompanyPostVO.getUsername());
-//        map.put("startDate", accompanyPostVO.getStartDate());
-//        map.put("lastDate", accompanyPostVO.getLastDate());
-//        map.put("tags", accompanyPostVO.getTags());
+        map.put("photoURL", accompanyPostVO.getPhotoURL());
+        map.put("username", accompanyPostVO.getUsername());
+        map.put("startDate", accompanyPostVO.getStartDate());
+        map.put("lastDate", accompanyPostVO.getLastDate());
+        map.put("tags", accompanyPostVO.getTags());
         map.put("content", accompanyPostVO.getContent());
         Log.d(TAG, map.toString());
 
